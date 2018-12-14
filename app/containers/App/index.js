ff075ee55589c48693c213c061775d188b7f0ef7
lowwor <lowwor@foxmail.com>
2018-12-14 15:46:00 +0800

    fix: fix global style

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -16,14 +16,7 @@ import indexRoutes from 'routes/index';
 import './material-dashboard-pro-react.css';
 import GlobalStyle from '../../global-styles';
 
-const AppWrapper = styled.div`
-  max-width: calc(768px + 16px * 2);
-  margin: 0 auto;
-  display: flex;
-  min-height: 100%;
-  padding: 0 16px;
-  flex-direction: column;
-`;
+const AppWrapper = styled.div``;
 
 export default class App extends React.Component {
   componentDidMount() {
--- app/global-styles.js
+++ app/global-styles.js
@@ -1,11 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-  html,
-  body {
-    height: 100%;
-    width: 100%;
-  }
 `;
 
 export default GlobalStyle;
